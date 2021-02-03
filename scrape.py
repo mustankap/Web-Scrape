790f5dfa1ab52852e353911ad349f128c01811d2
mustankap <you@example.com>
2021-02-03 22:19:53 +0530

    update

--- scrape.py
+++ scrape.py
@@ -4,8 +4,41 @@ import urllib.request,sys,time
 import requests
 import pandas as pd
 
-#url of the page that we want to Scarpe
-#+str() is used to convert int datatype of the page no. and concatenate that to a URL for pagination purposes.
-URL = 'https://www.politifact.com/factchecks/list/?page='+str(page)
-#Use the browser to get the URL. This is a suspicious command that might blow up.
-page = requests.get(url)
+
+URL = 'https://www.politifact.com/factchecks/list/'
+
+try:
+     # this might throw an exception if something goes wrong.
+     page=requests.get(URL) 
+     # this describes what to do if an exception is thrown 
+except Exception as e:    
+    
+    error_type, error_obj, error_info = sys.exc_info()      
+    
+    print ('ERROR FOR LINK:',url)
+                            
+    print (error_type, 'Line:', error_info.tb_lineno)
+    
+time.sleep(2)   
+    soup=BeautifulSoup(page.text,'html.parser')
+    frame=[]
+    links=soup.find_all('li',attrs={'class':'o-listicle__item'})
+    print(len(links))
+    filename="NEWS.csv"
+    f=open(filename,"w", encoding = 'utf-8')
+    headers="Statement,Link,Date, Source, Label\n"
+    f.write(headers)
+    
+    for j in links:
+        Statement = j.find("div",attrs={'class':'m-statement__quote'}).text.strip()
+        Link = "https://www.politifact.com"
+        Link += j.find("div",attrs={'class':'m-statement__quote'}).find('a')['href'].strip()
+        Date = j.find('div',attrs={'class':'m-statement__body'}).find('footer').text[-14:-1].strip()
+        Source = j.find('div', attrs={'class':'m-statement__meta'}).find('a').text.strip()
+        Label = j.find('div', attrs ={'class':'m-statement__content'}).find('img',attrs={'class':'c-image__original'}).get('alt').strip()
+        frame.append((Statement,Link,Date,Source,Label))
+        f.write(Statement.replace(",","^")+","+Link+","+Date.replace(",","^")+","+Source.replace(",","^")+","+Label.replace(",","^")+"\n")
+    upperframe.extend(frame)
+f.close()
+data=pd.DataFrame(upperframe, columns=['Statement','Link','Date','Source','Label'])
+data.head()
