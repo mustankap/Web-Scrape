85ad0e99a6224add47d7488a2928f2eab9017a9c
mustankap <mustankap@gmail.com>
2021-02-16 00:31:06 +0530

    update

---
+++ tickers_for_yahoo.py
@@ -0,0 +1,32 @@
+import urllib
+from bs4 import BeautifulSoup
+import urllib.request,sys,time
+import requests
+import pandas as pd
+
+pagesToGet= 1
+companies = []
+upperframe=[]  
+for page in range(1,pagesToGet+1):
+    # print('processing page :')
+    url = 'https://in.finance.yahoo.com/quote/%5ENSEI/components/'
+    # print(url)
+    
+    
+    try:
+        page=requests.get(url)                             # this might throw an exception if something goes wrong.
+    
+    except Exception as e:                                   # this describes what to do if an exception is thrown
+        error_type, error_obj, error_info = sys.exc_info()      # get the exception information
+        print ('ERROR FOR LINK:',url)                          #print the link that cause the problem
+        print (error_type, 'Line:', error_info.tb_lineno)     #print error info and line that threw the exception
+        continue                                              #ignore this page. Abandon this and go back.
+    time.sleep(2)   
+    soup = BeautifulSoup(page.text,'html.parser')
+    frame = []
+    links = soup.find_all('tr',attrs={'class':'BdT Bdc($seperatorColor) Ta(end) Fz(s)'})
+        
+    for j in links:
+        ticker = j.find('a').text.strip()
+        companies.append(ticker)
+# print(companies)
---
+++ yahoo_finance_for_each_company.py
@@ -0,0 +1,56 @@
+from tickers_for_yahoo import companies
+import requests
+import urllib
+from bs4 import BeautifulSoup
+import urllib.request,sys,time
+import requests
+import pandas as pd
+
+tickers = companies
+upperframe=[]
+filename = "YAHOONEWS.csv"
+f = open(filename,"w", encoding = 'utf-8')
+headers="Topic,Statement,Link\n"
+f.write(headers)
+
+for i in range(len(tickers)):
+    url = f'https://in.finance.yahoo.com/quote/{tickers[i]}/news?p={tickers[i]}'
+    #print(url)
+    try:
+        page=requests.get(url)  
+        # print(page)                           
+    except Exception as e:                                   
+        error_type, error_obj, error_info = sys.exc_info()      
+        print ('ERROR FOR LINK:',url)                          
+        print (error_type, 'Line:', error_info.tb_lineno)     
+        continue                                              
+    time.sleep(1)   
+    soup = BeautifulSoup(page.text,'html.parser')
+    frame = []
+    links = soup.find_all('li',attrs={'class':'js-stream-content Pos(r)'})
+    if len(links) == 0:
+        continue
+    else:
+        f.write(f'company :{tickers[i]}'+'\n')
+    for j in links:
+        try:
+            Topic = j.find('a').text.strip()
+            #print(Topic)
+            Statement = j.find('p').text.strip()
+            #print(Statement)
+            Link = 'www.in.finance.yahoo.com/news/'
+            Link += j.find('h3').find('a')['href'].strip()
+            #print(Link)
+            # Date = j.find('div').find('span')
+            # print(Date.text)
+            frame.append((Topic,Statement,Link))
+            f.write(Topic.replace(",","^")+","+Statement.replace(",","^")+","+Link+"\n")
+        except Exception as e:
+            continue
+    upperframe.extend(frame)
+    f.write('\n')
+f.close()
+data=pd.DataFrame(upperframe, columns=['Topic','Statement','Link'])
+data.head()
+            
+    
